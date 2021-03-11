430151c535d48b4e24165a0063f6912949ec8a20
danielbohnemann <dannybohnemann@yahoo.com>
2021-03-11 12:07:04 -0600

    calculator

---
+++ forms/calculator/calculator.js
@@ -0,0 +1,12 @@
+
+addBtn.onclick=function(){
+  let num1 = parseInt(fnInput.value)
+  let num2 = parseInt(snInput.value)
+  let sum = num1 + num2
+  lblMessage2.value = sum
+}
+
+
+resetBtn.onclick=function(){
+  lblMessage2.value = ""
+}
