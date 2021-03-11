e27b222d0aa733d81bc777c505ab007099f8466f
danielbohnemann <dannybohnemann@yahoo.com>
2021-03-11 11:56:38 -0600

    added problem 1

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,13 @@
-/*
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+
+btnSubmit.onclick=function(){
+  let name = inptName.value
+  if(members.includes(name) == true) { 
+  lblMessage.value = `Hello ${name} your name is on the list.`
+} else {
+  lblMessage.value = `Hello ${name}, your name is not on the list.`
+  members.push(name)
+  }
+  }
